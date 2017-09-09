4639ccbd91efe52874a732a15fd8bcfd0a0603fb
saramcgaughy <31710852+saramcgaughy@users.noreply.github.com>
2017-09-09 15:23:18 -0500

    Add files via upload

---
+++ McGaughy+Python+Notebook.py
@@ -0,0 +1,67 @@
+
+# coding: utf-8
+
+# In[1]:
+
+print("hello world")
+
+
+# In[2]:
+
+a=3+1
+
+
+# In[ ]:
+
+print(a)
+print("three plus one is",a)
+
+
+# In[4]:
+
+print(a)
+
+
+# In[5]:
+
+print("three plus one is", a)
+
+
+# In[6]:
+
+first_name="Sara"
+last_name="McGaughy"
+print(first,last)
+
+
+# In[7]:
+
+first_name="Sara"
+last_name="McGaughy"
+print(first_name,last_name)
+
+
+# In[8]:
+
+print("Sara","\\t","McGaughy","\n",first_name,last_name)
+
+
+# In[9]:
+
+print("Sara","\t","McGaughy","\n",first_name,last_name)
+
+
+# #This is a markdown cell
+
+# 1. My name is Sara McGaughy.
+# 2. I am interested in Data Science because learning more about it will help advance my career.
+# 3. I have very little programming experience.  I will do some SAS programming on the job.
+# 4. I took a Bachelor's-level stats class and a Master's-level stats class in an MBA program.  I do not use stats on the job.
+# 5. I have a little bit of experience with R from DS700.  I have absolutely no experience with Python.
+# 6. I hope to learn about R and Python in this class.
+# 7. I don't care to share anything else at this time, thank you.
+
+# In[ ]:
+
+
+
