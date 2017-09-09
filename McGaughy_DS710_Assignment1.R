c05b0336ada1fd6d3f93bbd4a2e8288443948baf
saramcgaughy <31710852+saramcgaughy@users.noreply.github.com>
2017-09-09 15:10:47 -0500

    Add files via upload
    Added R code in both a .txt and .R file.  I wasn't sure which file format was preferred.

---
+++ McGaughy_DS710_Assignment1.R
@@ -0,0 +1,53 @@
+
+R version 3.4.1 (2017-06-30) -- "Single Candle"
+Copyright (C) 2017 The R Foundation for Statistical Computing
+Platform: i386-w64-mingw32/i386 (32-bit)
+
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
+
+  Natural language support but running in an English locale
+
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
+
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
+
+[Previously saved workspace restored]
+
+> 2017^(1/3)
+[1] 12.63481
+> abs(5.7-6.8)/.58
+[1] 1.896552
+> a=1:12
+> a
+ [1]  1  2  3  4  5  6  7  8  9 10 11 12
+> b=c(1,3,5,7,9,11)
+> b
+[1]  1  3  5  7  9 11
+> c=seq(1,11,2)
+> c
+[1]  1  3  5  7  9 11
+> ln.a=log(a)
+> ln.a
+ [1] 0.0000000 0.6931472 1.0986123 1.3862944 1.6094379 1.7917595 1.9459101
+ [8] 2.0794415 2.1972246 2.3025851 2.3978953 2.4849066
+> b^(1/2)
+[1] 1.000000 1.732051 2.236068 2.645751 3.000000 3.316625
+> ?sd
+starting httpd help server ... done
+> Name=”Sara”
+Error: unexpected input in "Name=”"
+> Name
+Error: object 'Name' not found
+> Name = "Sara"
+> Name
+[1] "Sara"
+> paste ("My name is",Name)
+[1] "My name is Sara"
+> #The sd function computes the standard deviation of the values in x.
+> 
